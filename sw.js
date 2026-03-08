bca555fe028083427d0cdea794e643c608a93ef7
trickpa-cmd <ziadehpatrick@gmail.com>
2026-03-08 22:05:41 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,26 @@
+
+const CACHE_NAME = 'miaou-cache-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './index_confirm_delete.html',
+  './miaou.webmanifest',
+  './miaou-icon-192.png',
+  './miaou-icon-512.png'
+];
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))).then(() => self.clients.claim())
+  );
+});
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+    const copy = response.clone();
+    caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+    return response;
+  }).catch(() => cached)));
+});
